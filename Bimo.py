339d32cf9bdc3232b61c8012e8ff13b99ec5cbff
Bebop <nneven@usc.edu>
2023-08-09 21:05:05 -0700

    add router query engine, cleanup

--- Bimo.py
+++ Bimo.py
@@ -1,101 +1,16 @@
-import io, os
-import base64
-import dotenv
-import PyPDF2
 import streamlit as st
-dotenv.load_dotenv()
-from llama_index import (
-    ListIndex,
-    VectorStoreIndex,
-    SimpleDirectoryReader,
-    ServiceContext,
-    StorageContext,
-    LLMPredictor,
-    load_index_from_storage,
-    ComposableGraph,
-)
-from langchain.chat_models import ChatOpenAI
+from utils import QueryEngineFactory, PDFViewer
 
+COURSE = "CSCI-360"
 
-@st.cache_resource
-def initialize_index(index_name, documents_folder):
-    llm_predictor = LLMPredictor(
-        llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    )
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
-    if os.path.exists(index_name):
-        index = load_index_from_storage(
-            StorageContext.from_defaults(persist_dir=index_name),
-            service_context=service_context,
-        )
-    else:
-        documents = SimpleDirectoryReader(documents_folder).load_data()
-        index = VectorStoreIndex.from_documents(
-            documents, service_context=service_context
-        )
-        index.storage_context.persist(persist_dir=index_name)
+query_engine_factory = QueryEngineFactory(COURSE)
+router_query_engine = query_engine_factory.create_query_engine("router")
 
-    return index
-
-
-# @st.cache_data(max_entries=100, persist=True)
-# def query_index(_index, query_text):
-#     if _index is None:
-#         return "Please initialize the index!"
-#     response = _index.as_query_engine().query(query_text)
-#     return response
-
-
-def display_sources(response):
-    for source in response.source_nodes:
-        # Get PDF file name and page number from metadata
-        if 'file_name' in source.node.metadata and 'page_label' in source.node.metadata:
-            pdf_file = ""
-            if "ENST 100" in source.node.metadata['file_name']:
-                pdf_file = "documents/lectures/" + source.node.metadata['file_name']
-            elif "ENST 100" not in source.node.metadata['file_name']:
-                pdf_file = "documents/textbooks/" + source.node.metadata['file_name']
-            pdf_page = int(source.node.metadata['page_label']) - 1  # PyPDF2 uses 0-based indexing
-
-            # Open the PDF 
-            with open(pdf_file, 'rb') as file:
-                pdf_reader = PyPDF2.PdfReader(file)
-                pdf_writer = PyPDF2.PdfWriter()
-                pdf_writer.add_page(pdf_reader.pages[pdf_page])
-                
-                pdf_bytes = io.BytesIO()
-                pdf_writer.write(pdf_bytes)
-                pdf_bytes = pdf_bytes.getvalue()
-
-            # Encode the PDF bytes to base64 and create HTML link
-            base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
-            pdf_file_html_link = f'<embed src="data:application/pdf;base64,{base64_pdf}#page={pdf_page}" width="700" height="500" type="application/pdf">'
-
-            # Display the PDF page in Streamlit
-            st.markdown(pdf_file_html_link, unsafe_allow_html=True)
-            st.markdown("")
-            st.markdown(source.node.metadata)
-            st.markdown(source.score)
-            st.markdown(source.node.text)
-
-    # st.markdown(response.get_formatted_sources())
-
-
-st.title("🦙 Bimo Demo 🦙")
-st.subheader("Ask a question about ENST 100")
-
-lecture_index = initialize_index("indices/lectures", "documents/lectures")
-textbook_index = initialize_index("indices/textbooks", "documents/textbooks")
-index_summaries = ["Lecture notes about environmental science", "Textbook about environmental science"]
-
-graph = ComposableGraph.from_indices(ListIndex, [lecture_index, textbook_index], index_summaries)
-query_engine = graph.as_query_engine()
-
-text = st.text_input("Query:", value="What is ecosystem and ecosystem ecology?")
+st.title("Query Engine Demo 🔍")
+text = st.text_input(f"Ask me anything about {COURSE}!")
 
 if st.button("Run Query") and text is not None:
-    # response = query_index(index, text)
-    response = query_engine.query(text)
+    response = router_query_engine.query(text)
 
     st.divider()
     st.caption("Response:")
@@ -103,15 +18,4 @@ if st.button("Run Query") and text is not None:
 
     st.divider()
     st.caption("Sources:")
-    display_sources(response)
-
-    # llm_col, embed_col = st.columns(2)
-    # with llm_col:
-    #     st.markdown(
-    #         f"LLM Tokens Used: {index.service_context.llm_predictor._last_token_usage}"
-    #     )
-
-    # with embed_col:
-    #     st.markdown(
-    #         f"Embedding Tokens Used: {index.service_context.embed_model._last_token_usage}"
-    #     )
+    PDFViewer.display_sources(response)
---
+++ utils.py
@@ -0,0 +1,123 @@
+import os
+import sys
+import logging
+import dotenv
+import PyPDF2
+import base64
+import streamlit as st
+from io import BytesIO
+
+dotenv.load_dotenv()
+
+from llama_index import (
+    ListIndex,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    ServiceContext,
+    StorageContext,
+    load_index_from_storage,
+    LLMPredictor,
+)
+from llama_index.selectors.pydantic_selectors import (
+    # PydanticMultiSelector,
+    PydanticSingleSelector,
+)
+from llama_index.indices.postprocessor import (
+    SentenceTransformerRerank,
+    PrevNextNodePostprocessor,
+)
+from llama_index.tools.query_engine import QueryEngineTool
+from llama_index.query_engine.router_query_engine import RouterQueryEngine
+from llama_index.indices.query.query_transform.base import HyDEQueryTransform
+from llama_index.query_engine.transform_query_engine import TransformQueryEngine
+from langchain.chat_models import ChatOpenAI
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().handlers = [logging.StreamHandler(sys.stdout)]
+
+LLM = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+class QueryEngineFactory:
+    def __init__(self, course):
+        self.course = course
+
+    def create_index(self, index_type, folder_name):
+        index_path = f"courses/{self.course}/indices/{folder_name}/"
+        documents_path = f"courses/{self.course}/{folder_name}/"
+
+        llm_predictor = LLMPredictor(llm=LLM)
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+
+        if os.path.exists(index_path):
+            return load_index_from_storage(
+                StorageContext.from_defaults(persist_dir=index_path),
+                service_context=service_context,
+            )
+
+        documents = SimpleDirectoryReader(documents_path).load_data()
+        for document in documents:
+            document.metadata["path"] = documents_path
+
+        if index_type == "list":
+            index = ListIndex.from_documents(documents=documents, service_context=service_context)
+            index.storage_context.persist(persist_dir=index_path)
+        elif index_type == "vector":
+            index = VectorStoreIndex.from_documents(documents=documents, service_context=service_context)
+            index.storage_context.persist(persist_dir=index_path)
+        else:
+            raise ValueError("Invalid index type!")
+        
+        return index
+
+    def create_query_engine(self, name):
+        if name == "syllabus":
+            index = self.create_index(index_type="list", folder_name=name)
+            postprocessor = SentenceTransformerRerank(model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=2)
+            query_engine = index.as_query_engine(node_postprocessors=[postprocessor])
+        elif name == "lectures":
+            # query_transform = HyDEQueryTransform()
+            index = self.create_index(index_type="vector", folder_name=name)
+            postprocessor = PrevNextNodePostprocessor(docstore=index.docstore, num_nodes=1, mode="both")
+            query_engine = index.as_query_engine(node_postprocessors=[postprocessor])
+        elif name == "router":
+            syllabus_tool = QueryEngineTool.from_defaults(
+                query_engine=self.create_query_engine("syllabus"),
+                description="Queries information from the course syllabus including: course information, learning objectives, required materials, assignments, grading, policies, dates, schedule, etc.",
+            )
+            lecture_tool = QueryEngineTool.from_defaults(
+                query_engine=self.create_query_engine("lectures"),
+                description="Queries information from the course lecture slides including: subject knowledge, topics, concepts, definitions, examples, etc.",
+            )
+            query_engine = RouterQueryEngine(
+                selector=PydanticSingleSelector.from_defaults(),
+                query_engine_tools=[syllabus_tool, lecture_tool],
+            )
+        else:
+            raise ValueError("Invalid query engine name!")
+        
+        return query_engine
+
+
+class PDFViewer:
+    @staticmethod
+    def display_sources(response):
+        for source in response.source_nodes:
+            pdf_file_html_link = PDFViewer._get_pdf_file_html_link(source)
+            st.markdown(pdf_file_html_link, unsafe_allow_html=True)
+            st.json(source.node.metadata)
+
+    @staticmethod
+    def _get_pdf_file_html_link(source):
+        pdf_file = source.node.metadata["path"] + source.node.metadata["file_name"]
+        pdf_page = int(source.node.metadata["page_label"]) - 1
+        source.node.metadata["score"] = source.score
+
+        with open(pdf_file, "rb") as file:
+            pdf_reader = PyPDF2.PdfReader(file)
+            pdf_writer = PyPDF2.PdfWriter()
+            pdf_writer.add_page(pdf_reader.pages[pdf_page])
+            pdf_bytes = BytesIO()
+            pdf_writer.write(pdf_bytes)
+            base64_pdf = base64.b64encode(pdf_bytes.getvalue()).decode("utf-8")
+
+        return f'<embed src="data:application/pdf;base64,{base64_pdf}#page={pdf_page}" width="700" height="500" type="application/pdf">'
